625ad833bd2c5db6b66fc6932bbd2636fa101ddb
STAN-65031 <lox.ibany@gmail.com>
2022-10-07 17:44:16 +0300

    corrected the mistake 2

--- task2.py
+++ task2.py
@@ -13,8 +13,25 @@
 # out
 # [2, 2, 4, 8, 8]
 # [16, 16, 4]
-import task1
+from random import sample
 
+def input_numbers(inputText):
+    okey = False
+    while not okey:
+        try:
+            number = int(input(f"{inputText}"))
+            okey = True
+            if number == 0:
+                okey = False
+                print("Список не может быть равен 0")
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def creates_list(number):
+    list_nums = sample(range(1, number * 5), number)
+    return list_nums
 
 def multiplies_numbers(used_list):
     multiply_numbers = []
@@ -33,8 +50,8 @@ def multiplies_numbers(used_list):
     return multiply_numbers
 
 
-num = task1.input_numbers("Задайте длинну списка: ")
-used_list = task1.creates_list(num)
+num = input_numbers("Задайте длинну списка: ")
+used_list = creates_list(num)
 print(used_list)
 result = multiplies_numbers(used_list)
 print(f"произведение пар чисел списка: {result}")
